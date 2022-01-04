f1e12e51c78b2aa2b21e04a255907cc47a9f44e0
Selenuix <anthony.ciszek@outlook.com>
2022-01-04 16:43:41 +0100

    Add: GeneralController, routes

---
+++ src/Controller/GeneralController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GeneralController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('index.html.twig', [
+            'controller_name' => 'GeneralController',
+        ]);
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(): Response
+    {
+        return $this->render('about.html.twig', [
+            'controller_name' => 'GeneralController',
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="about")
+     */
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig', [
+            'controller_name' => 'GeneralController',
+        ]);
+    }
+}
