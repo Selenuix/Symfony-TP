9190c343c1e224f02d1406a63e46955f1efe8f30
Selenuix <anthony.ciszek@outlook.com>
2022-01-07 17:19:02 +0100

    Add: contact form

--- src/Controller/GeneralController.php
+++ src/Controller/GeneralController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Project;
 
@@ -28,18 +32,36 @@ class GeneralController extends AbstractController
      */
     public function about(): Response
     {
-        return $this->render('about.html.twig', [
-            'controller_name' => 'GeneralController',
-        ]);
+        return $this->render('about.html.twig');
     }
 
-    /**
-     * @Route("/contact", name="contact")
-     */
-    public function contact(): Response
+	/**
+	 * @Route("/contact", name="contact")
+	 * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
+	 */
+    public function contact(Request $request, MailerInterface $mailer): Response
     {
-        return $this->render('contact.html.twig', [
-            'controller_name' => 'GeneralController',
-        ]);
+	    $form = $this->createForm(ContactType::class);
+
+	    $form->handleRequest($request);
+	    if ($form->isSubmitted() && $form->isValid()) {
+		    $contactFormData = $form->getData();
+
+		    $message = (new Email())
+			    ->from($contactFormData['email'])
+			    ->to('selenuix.temp@gmail.com')
+			    ->subject('You got mail')
+			    ->text('Sender : '.$contactFormData['email'].\PHP_EOL. $contactFormData['message'],
+				    'text/plain');
+		    $mailer->send($message);
+
+		    $this->addFlash('success', 'Your message has been sent');
+
+		    return $this->redirectToRoute('contact');
+	    }
+
+	    return $this->render('contact.html.twig', [
+		    'form' => $form->createView(),
+	    ]);
     }
 }
